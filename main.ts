6948af276e74caeb6ad21242887b7cd26a196c2d
Azlankhan12345 <161835064+Azlankhan12345@users.noreply.github.com>
2024-03-26 14:33:39 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+async function main() {
+    const answer = await inquirer.prompt([
+        { message: "Enter your first number", type: "number", name: "firstName" },
+        { message: "Enter your second number", type: "number", name: "secondName" },
+        {
+            message: "Select one of the operators to perform action",
+            type: "list",
+            name: "operator",
+            choices: ["ADDITION", "SUBTRACTION", "MULTIPLICATION", "DIVISION"],
+        }
+    ]);
+
+    // CONDITION STATEMENT
+    if (answer.operator === "ADDITION") {
+        console.log(answer.firstName + answer.secondName);
+    } else if (answer.operator === "SUBTRACTION") {
+        console.log(answer.firstName - answer.secondName);
+    } else if (answer.operator === "MULTIPLICATION") {
+        console.log(answer.firstName * answer.secondName);
+    } else if (answer.operator === "DIVISION") {
+        console.log(answer.firstName / answer.secondName);
+    } else {
+        console.log("Please select a correct operator.");
+    }
+}
+
+main();
